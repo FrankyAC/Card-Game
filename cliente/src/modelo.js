43a12992656fb38a089aea73a1390b5c8d7b1641
FRANCISCO JOSE ALFARO CANADAS <FcoJose.Alfaro2@alu.uclm.es>
2018-10-15 11:54:30 +0200

    Version TravisCI

--- cliente/src/modelo.js
+++ cliente/src/modelo.js
@@ -6,7 +6,7 @@ function Juego(){
 		this.cartas.push(carta);
 	}
 	this.agregarUsuario=function(usuario){
-		usuario.mazo=this.crearColeccion();
+		usuario.mazo=_.shuffle(this.crearColeccion());
 		usuario.juego=this;
 		this.usuarios.push(usuario);
 	}
@@ -61,20 +61,30 @@ function Partida(nombre){
 	this.comprobarInicio=function(){
 		if (this.usuariosPartida.length==2){
 			this.turnoInicial();
+			this.asignarManoInicial();
+		}
+	}
+	this.asignarManoInicial=function(){
+		for(var i=0;i<this.usuariosPartida.length;i++){
+			this.usuariosPartida[i].manoInicial();
 		}
 	}
 	this.turnoInicial=function(){
 		var num=Math.round(Math.random());
 		this.usuariosPartida[num].esMiTurno();
+
 	}
 	this.cambiarTurno=function(){
-		if (this.usuariosPartida[0].turno){
-			this.usuariosPartida[0].turno=false;
-			this.usuariosPartida[1].esMiTurno()
-		}else{
-			this.usuariosPartida[1].turno=false;
-			this.usuariosPartida[0].esMiTurno();
+		for(var i=0;i<this.usuariosPartida.length;i++){
+			this.usuariosPartida[i].cambiarTurno();
 		}
+		// if (this.usuariosPartida[0].turno){
+		// 	this.usuariosPartida[0].turno=false;
+		// 	this.usuariosPartida[1].esMiTurno();
+		// }else{
+		// 	this.usuariosPartida[1].turno=false;
+		// 	this.usuariosPartida[0].esMiTurno();
+		// }
 	}
 	this.crearTablero();
 }
@@ -117,14 +127,55 @@ function Zona(nombre){
 	}
 }
 
+function MiTurno(){
+	this.pasarTurno=function(usr){
+		usr.partida.cambiarTurno();
+	}
+	this.jugarCarta=function(usr,carta){
+		usr.puedeJugarCarta(carta);
+	}
+	this.cambiarTurno=function(usr){
+		usr.turno=new NoMiTurno();
+	}
+	this.meToca=function(){
+		return true;
+	}
+}
+
+function NoMiTurno(){
+	this.esMiTurno=function(usr){
+		usr.turno=new MiTurno();
+		usr.cogerCarta();
+		usr.elixir=usr.consumido+1;
+		usr.consumido=0;
+	}
+	this.pasarTurno=function(usr){
+		console.log("No se puede pasar el turno si no se tiene");
+	}
+	this.jugarCarta=function(carta,usr){
+		console.log("No es tu turno");
+	}
+	this.cambiarTurno=function(usr){
+		usr.turno=new MiTurno();
+		this.esMiTurno(usr);
+	}
+	this.meToca=function(){
+		return false;
+	}
+}
+
 function Usuario(nombre){
 	this.nombre=nombre;
 	this.juego=undefined;
+	this.vidas=20;
 	this.mazo=[];
-	this.mano=[];
-	this.turno=false;
+	//this.mano=[];
+	//this.ataque=[];
+	this.elixir=1;
+	this.turno=new NoMiTurno();
 	this.zona=undefined;
 	this.partida=undefined;
+	this.consumido=0;
 	this.asignarPartida=function(partida){
 		this.partida=partida;
 	}
@@ -137,16 +188,59 @@ function Usuario(nombre){
 	this.eligePartida=function(nombre){
 		this.juego.asignarPartida(nombre,this);
 	}
+	this.cambiarTurno=function(){
+		this.turno.cambiarTurno(this);
+	}
 	this.pasarTurno=function(){
-		this.partida.cambiarTurno();
+		//this.partida.cambiarTurno();
+		this.turno.pasarTurno(this);
 	}
 	this.esMiTurno=function(){
-		this.turno=true;
-		this.cogerCarta();
+		this.turno.esMiTurno(this);
+		// this.turno=true;
+		// this.cogerCarta();
+		// this.elixir=this.consumido+1;
+		// this.consumido=0;
 	}
 	this.cogerCarta=function(){
-		return this.mazo.find(function(each){
+		var carta;
+		carta= this.mazo.find(function(each){
 			return each.posicion=="mazo";
+		});
+		carta.posicion="mano";
+	}
+	this.jugarCarta=function(carta){
+		this.turno.jugarCarta(this,carta);
+	}
+	this.puedeJugarCarta=function(carta){
+		if (this.elixir>=carta.coste){
+			carta.posicion="ataque";
+			this.elixir=this.elixir-carta.coste;
+			this.consumido=this.consumido+carta.coste;
+		}
+		else
+			console.log("No tienes suficiente elixir");
+	}
+	this.ataque=function(carta,objetivo){
+		objetivo.esAtacado(carta);
+	}
+	this.esAtacado=function(carta){
+		this.vidas=this.vidas-carta.ataque;
+		this.comprobarVidas();
+	}
+	this.comprobarVidas=function(){
+		if (this.vidas<=0){
+			this.juego.fin();
+		}
+	}
+	this.manoInicial=function(){
+		for(var i=0;i<5;i++){
+			this.cogerCarta();
+		}
+	}
+	this.localizarCarta=function(coste){
+		return this.mazo.find(function(each){
+			return each.posicion=="mano" && each.coste==coste;
 		});
 	}
 }
@@ -157,4 +251,14 @@ function Carta(vidas,ataque,nombre,coste){
 	this.nombre=nombre;
 	this.coste=coste;
 	this.posicion="mazo";
+	this.esAtacado=function(carta){
+		this.vidas=this.vidas-carta.ataque;
+		this.comprobarVidas();
+		carta.comprobarVidas();
+	}
+	this.comprobarVidas=function(){
+		if (this.vidas<=0){
+			this.posicion="cementerio";
+		}
+	}
 }
